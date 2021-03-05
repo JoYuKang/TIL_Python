a7e0351f4c4505d03b0f1e70ea2aba52ae57b103
Yu Knag <ykang9533@naver.com>
2021-03-05 12:10:54 +0900

    Python_CODE

---
+++ 정다면체.py
@@ -0,0 +1,15 @@
+
+a,b = map(int,input().split())
+
+n =[0]*(a+b+1)
+for i in range(1,a+1):
+    for j in range(1,b+1):
+        n[i+j] +=1
+max = 0
+for i in range(1,a+b):
+    if max <= n[i]:
+        max = n[i]
+
+for i in range(a+b):
+    if max ==n[i]:
+        print(i,end=' ')
