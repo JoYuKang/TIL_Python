795ef085e2ea83f9951ae44fe83606ebd73d8e12
JoYuKang <46198324+JoYuKang@users.noreply.github.com>
2025-04-20 23:26:17 +0900

    Update 네트워크.py

--- 네트워크.py
+++ 네트워크.py
@@ -1,17 +1,18 @@
 def solution(n, computers):
+    visited = [False] * n
     answer = 0
-    visited = [0 for _ in range(n)]
-    for com in range(n):
-        if visited[com] == 0:
-            DFS(n, computers, com, visited)
-            answer += 1 #DFS로 최대한 컴퓨터들을 방문하고 빠져나오게 되면 그것이 하나의 네트워크.
+
+    def dfs(pc):
+        visited[pc] = True
+        for idx, c in enumerate(computers[pc]):  #
+            if c and not visited[idx]:
+                dfs(idx)
+
+    for pc in range(n):
+        if not visited[pc]:
+            dfs(pc)
+            answer += 1
     return answer
 
-def DFS(n, computers, com, visited):
-    visited[com] = 1
-    for connect in range(n):
-        if connect != com and computers[com][connect] == 1: #연결된 컴퓨터
-            if visited[connect] == 0:
-                DFS(n, computers, connect, visited)
 
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]	))
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]))
